bc72eab0bc42ab9236d07e51ffc6c605dca49a43
Zaryab Khan <93703454+Zaryab13@users.noreply.github.com>
2023-08-08 19:06:18 +0500

    Coverted the Code form styles-comp to css.modules

--- src/components/Todos/todoForm/TodoForm.js
+++ src/components/Todos/todoForm/TodoForm.js
@@ -1,43 +1,43 @@
 import React , { useState } from "react";
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import Button from "../../UI/Button/Button";
-import "./TodoForm.css";
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 4rem 2rem;
-  width: 30rem;
-  max-width: 90%;
-  margin: 20px auto;
-  box-shadow: rgba(0, 0, 0 , 20%) 0px 7px 29px 0px;
-`;
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+import styles from "./TodoForm.module.css";
+// const Form = styled.form`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: flex-start;
+//   padding: 4rem 2rem;
+//   width: 30rem;
+//   max-width: 90%;
+//   margin: 20px auto;
+//   box-shadow: rgba(0, 0, 0 , 20%) 0px 7px 29px 0px;
+// `;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label{
-    font-size: 1.2rem;
-    margin: 20px 0;
-    font-weight: 500;
-    color: ${props => (props.invalid ? 'red' : 'black')};
-  }
+//   & label{
+//     font-size: 1.2rem;
+//     margin: 20px 0;
+//     font-weight: 500;
+//     color: ${props => (props.invalid ? 'red' : 'black')};
+//   }
 
-  & input{
-    font-family: inherit;
-    width: 100%;
-    padding: 0.5rem;
-    margin: 10px 0;
-    background: ${props => (props.invalid ? '#ffd7d7' : 'rgb(207, 205, 205)')};
-    border: none;
-    outline: none;
-    color: #fff;
-    border-radius: 10px;
-    transition: background-color 0.3s ease-in-out;
-  }
-  & input:focus{
-    color: #000;
-  }
-`;
+//   & input{
+//     font-family: inherit;
+//     width: 100%;
+//     padding: 0.5rem;
+//     margin: 10px 0;
+//     background: ${props => (props.invalid ? '#ffd7d7' : 'rgb(207, 205, 205)')};
+//     border: none;
+//     outline: none;
+//     color: #fff;
+//     border-radius: 10px;
+//     transition: background-color 0.3s ease-in-out;
+//   }
+//   & input:focus{
+//     color: #000;
+//   }
+// `;
 const TodoForm = (props) => {
 
   const [enteredValue, setEnteredValue] = useState('');
@@ -64,13 +64,13 @@ const TodoForm = (props) => {
   };
   
   return (
-    <Form className="todo-form" onSubmit={TaskFormHandler}>
-      <FormControl invalid={!isValid}>
+    <form className={styles['todo-form']} onSubmit={TaskFormHandler}>
+      <div className={`${styles['form-control']} ${!isValid? styles['invalid'] : ''}`}>
         <label>Todo Name</label>
         <input type="text" value={enteredValue} onChange={TaskChangeHandler}/>
-      </FormControl>
+      </div>
       <Button type="submit">Add New Task</Button>
-    </Form>
+    </form>
   );
 };
 
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,43 +1,48 @@
-import styled from 'styled-components';
+import styles from './Button.module.css';
 
-const Button = styled.button`
-    margin: 10px 0;
-    font-family: inherit;
-    padding: 7px 30px;
-    border-radius: 50px;
-    border: 0;
-    background-color: rgb(0 0 0 / 20%);
-    box-shadow: rgb(0 0 0 / 5%) 0 0 8px;
-    letter-spacing: 1.5px;
-    text-transform: uppercase;
-    font-size: 10px;
-    transition: all .5s ease;
-    cursor: pointer;
+// import styled from 'styled-components';
 
+// const Button = styled.button`
+//     width: 100%;
+//     margin: 10px 0;
+//     font-family: inherit;
+//     padding: 7px 30px;
+//     border-radius: 50px;
+//     border: 0;
+//     background-color: rgb(0 0 0 / 20%);
+//     box-shadow: rgb(0 0 0 / 10%) 0 0 8px;
+//     letter-spacing: 1.5px;
+//     text-transform: uppercase;
+//     font-size: 10px;
+//     transition: all .5s ease;
+//     cursor: pointer;
+//     @media (min-width: 768px){
+//         width: auto;
+//     }
+    
+//     &:hover {
+//     letter-spacing: 3px;
+//     background-color: hsl(27, 80%, 48%);
+//     color: hsl(0, 0%, 100%);
+//     box-shadow: rgb(220, 113, 24) 0px 7px 29px 0px;
+//    }
    
-    &:hover {
-    letter-spacing: 3px;
-    background-color: hsl(27, 80%, 48%);
-    color: hsl(0, 0%, 100%);
-    box-shadow: rgb(220, 113, 24) 0px 7px 29px 0px;
-   }
-   
-    &:active {
-    letter-spacing: 3px;
-    background-color: hsl(27, 80%, 48%);
-    color: hsl(0, 0%, 100%);
-    box-shadow: rgb(220, 113, 24) 0px 0px 0px 0px;
-    transform: translateY(10px);
-    transition: 100ms;
-}
-`;
-
-// const Button = (props) => {
-//   return (
-//     <Button type={props.type}>
-//         {props.children}
-//     </Button>
-//   )
+//     &:active {
+//     letter-spacing: 3px;
+//     background-color: hsl(27, 80%, 48%);
+//     color: hsl(0, 0%, 100%);
+//     box-shadow: rgb(220, 113, 24) 0px 0px 0px 0px;
+//     transform: translateY(10px);
+//     transition: 100ms;
 // }
+// `;
+
+const Button = (props) => {
+  return (
+    <button className={styles['button']} type={props.type}>
+        {props.children}
+    </button>
+  )
+}
 
 export default Button;
